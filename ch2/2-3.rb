7ffdd951b6889ab47b4ddf4497b6eabab8030008
TAMURA Yoshiya <a@qmu.jp>
2021-01-21 19:35:21 +0900

    2-3.rb

---
+++ ch2/2-3.rb
@@ -0,0 +1,152 @@
+
+#---------------------------------
+# Literals & Operations
+#---------------------------------
+
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    false
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    true
+  end
+  def reduce(environment)
+    if left.reducible?
+      Add.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Add.new(left, right.reduce(environment))
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    true
+  end
+  def reduce(environment)
+    if left.reducible?
+      Multiply.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce(environment))
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+end
+
+class Boolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    false
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    true
+  end
+  def reduce(environment)
+    if left.reducible?
+      LessThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+end
+
+class Variable < Struct.new(:name)
+  def to_s
+    name.to_s
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    true
+  end
+  def reduce(environment)
+    environment[name]
+  end
+end
+
+#---------------------------------
+# Statement
+#---------------------------------
+
+class DoNothing
+  def to_s
+    'do-nothing'
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def ==(other_statement)
+    other_statement.instance_of?(DoNothing)
+  end
+  def reducible?
+    false
+  end
+end
+
+#---------------------------------
+# Abstract Machine
+#---------------------------------
+
+class Machine < Struct.new(:expression, :environment)
+  def step
+    self.expression = expression.reduce(environment)
+  end
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+    puts expression
+  end
+end
+
+#---------------------------------
+# Debug
+#---------------------------------
+
+exp = Add.new(Variable.new(:x), Variable.new(:y))
+vars = { x: Number.new(3), y: Number.new(4) }
+p Machine.new(exp, vars).run
+
+
