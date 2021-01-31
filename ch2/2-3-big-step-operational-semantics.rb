02b2c18c1c7f7be349dadbd40db0cf346fe03674
TAMURA Yoshiya <a@qmu.jp>
2021-01-31 15:23:42 +0900

    Done 2-3-big-step-operational-semantics.rb

---
+++ ch2/2-3-big-step-operational-semantics.rb
@@ -0,0 +1,157 @@
+
+#---------------------------------
+# Literals & Operations
+#---------------------------------
+
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    self
+  end
+end
+
+class Boolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    self
+  end
+end
+
+class Variable < Struct.new(:name)
+  def to_s
+    name.to_s
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    environment[name]
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
+
+class Assign < Struct.new(:name, :expression)
+  def to_s
+    "#{name} = #{expression}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    environment.merge({ name => expression.evaluate(environment)})
+  end
+end
+
+class DoNothing
+  def evaluate
+    environment
+  end
+end
+
+class If < Struct.new(:condition, :consequence, :alternative)
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def reducible?
+    true
+  end
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      consequence.evaluate(environment)
+    when Boolean.new(false)
+      alternative.evaluate(environment)
+    end
+  end
+end
+
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+  def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    second.evaluate(first.evaluate(environment))
+  end
+end
+
+class While < Struct.new(:condition, :body)
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+   def inspect
+    "<<#{self}>>"
+  end
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      evaluate(body.evaluate(environment))
+    when Boolean.new(false)
+      environment
+    end
+  end
+end
+
+
+#---------------------------------
+# Debug
+#---------------------------------
+
+
+st = While.new(
+  LessThan.new(Variable.new(:x), Number.new(5)),
+  Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+)
+
+p st
+p st.evaluate({ x: Number.new(1) })
